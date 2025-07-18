e2943eb2cae14e5a8c1a883a06fa93f9052a2631
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2025-07-19 01:37:42 +0530

    Add a global exception handle, to get specific parts of error

---
+++ patient-service/src/main/java/com/pm/patient_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.pm.patient_service.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// handle cross cutting concerns like  error handling inside  both controller and services
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex){
+
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
