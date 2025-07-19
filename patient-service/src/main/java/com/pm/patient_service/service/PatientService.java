c7f1cab8e9270a8ea985b5c395fa5079e17d15e5
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2025-07-19 22:34:57 +0530

    Add a delete patient endpoint

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -61,4 +61,8 @@ public class PatientService {
         return PatientMapper.toDTO(newPatient);
     }
 
+    public void deletePatient(UUID id){
+        patientRepository.deleteById(id);
+    }
+
 }
