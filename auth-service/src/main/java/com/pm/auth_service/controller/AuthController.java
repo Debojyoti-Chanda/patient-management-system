9039b5477c3cb03b845df487b9c74cf756e8c6f2
Debojyoti Chanda <chanda3debojyoti@gmail.com>
2025-07-22 23:41:56 +0530

    Create the auth-controller, with endpoint POST /login that returns a token

---
+++ auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.pm.auth_service.controller;
+
+import com.pm.auth_service.dto.LoginRequestDTO;
+import com.pm.auth_service.dto.LoginResponseDTO;
+import com.pm.auth_service.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Generate token on user login")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO){
+        //The authenticate method is expected to return an Optional<String>, which will contain the JWT token if
+        // authentication is successful, or be empty if it fails (e.g., incorrect credentials).
+        Optional<String> tokenOptional = authService.authenticate(loginRequestDTO);
+        if(tokenOptional.isEmpty() ){
+            // .build(): Creates a ResponseEntity with no body, just the status code. This informs the client that their
+            // authentication attempt was unsuccessful.
+             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String token = tokenOptional.get();
+        return ResponseEntity.ok().body(new LoginResponseDTO(token));
+    }
+}
